6c3271002414a03c1799c13c1f6426fe42012644
Raphael <raphael@corteks.fr>
2020-08-28 15:54:26 +0200

    randomizer is not static anymor and need to be injected in generator. Generator can pass a seed to randomizer.

--- src/generation/generator.ts
+++ src/generation/generator.ts
@@ -6,11 +6,19 @@ import { Table } from '../table-descriptor.interface';
 import { Logger } from 'log4js';
 
 export class Generator {
+    private randomizer: Randomizer;
+
     constructor(
         private dbConnector: DatabaseConnector,
         private schema: Schema,
         private logger: Logger
-    ) { }
+    ) {
+        this.randomizer = new Randomizer();
+    }
+
+    setSeed(seed: number) {
+        this.randomizer = new Randomizer(seed);
+    }
 
     private async empty(table: Table) {
         this.logger.info('empty: ', table.name);
@@ -42,12 +50,12 @@ export class Generator {
     public async fill(table: Table, reset: boolean = false, globalDisableTriggers: boolean = false) {
         if (reset) await this.empty(table);
         this.logger.info('fill: ', table.name);
-        let handleTriggers = table.disableTriggers || (table.disableTriggers === undefined && globalDisableTriggers)
-        if(handleTriggers) await this.dbConnector.disableTriggers(table.name);
+        let handleTriggers = table.disableTriggers || (table.disableTriggers === undefined && globalDisableTriggers);
+        if (handleTriggers) await this.dbConnector.disableTriggers(table.name);
         await this.before(table);
         await this.generateData(table);
         await this.after(table);
-        if(handleTriggers) await this.dbConnector.enableTriggers(table.name);
+        if (handleTriggers) await this.dbConnector.enableTriggers(table.name);
     }
 
     private async before(table: Table) {
@@ -91,9 +99,9 @@ export class Generator {
                     if (column.options.autoIncrement) continue;
                     if (column.values) {
                         if (Array.isArray(column.values)) {
-                            row[column.name] = column.values[Randomizer.randomInt(0, column.values.length - 1)];
+                            row[column.name] = column.values[this.randomizer.randomInt(0, column.values.length - 1)];
                         } else if (typeof column.values === 'string') {
-                            row[column.name] = this.schema.settings.values[column.values][Randomizer.randomInt(0, this.schema.settings.values[column.values].length - 1)];
+                            row[column.name] = this.schema.settings.values[column.values][this.randomizer.randomInt(0, this.schema.settings.values[column.values].length - 1)];
                         } else {
                             let valuesWithRatio: string[] = [];
                             Object.keys(column.values).forEach((key: string) => {
@@ -101,7 +109,7 @@ export class Generator {
                                 arr = arr.fill(key);
                                 valuesWithRatio = valuesWithRatio.concat(arr);
                             });
-                            row[column.name] = valuesWithRatio[Randomizer.randomInt(0, valuesWithRatio.length - 1)];
+                            row[column.name] = valuesWithRatio[this.randomizer.randomInt(0, valuesWithRatio.length - 1)];
                         }
                         continue;
                     }
@@ -112,37 +120,37 @@ export class Generator {
                     }
                     switch (column.generator) {
                         case 'bit':
-                            row[column.name] = Randomizer.randomBit(column.options.max);
+                            row[column.name] = this.randomizer.randomBit(column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'tinyint':
-                            row[column.name] = Randomizer.randomInt(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomInt(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'bool':
                         case 'boolean':
-                            row[column.name] = Randomizer.randomInt(0, 1);
+                            row[column.name] = this.randomizer.randomInt(0, 1);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'smallint':
-                            row[column.name] = Randomizer.randomInt(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomInt(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'mediumint':
-                            row[column.name] = Randomizer.randomInt(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomInt(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'int':
                         case 'integer':
                         case 'bigint':
-                            row[column.name] = Randomizer.randomInt(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomInt(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'decimal':
                         case 'dec':
                         case 'float':
                         case 'double':
-                            row[column.name] = Randomizer.randomFloat(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomFloat(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'date':
@@ -150,18 +158,18 @@ export class Generator {
                         case 'timestamp':
                             const min = column.options.min ? new Date(column.options.min) : new Date('01-01-1970');
                             const max = column.options.max ? new Date(column.options.max) : new Date();
-                            row[column.name] = Randomizer.randomDate(min, max);
+                            row[column.name] = this.randomizer.randomDate(min, max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'time':
-                            const hours = Randomizer.randomInt(-838, +838);
-                            const minutes = Randomizer.randomInt(-0, +59);
-                            const seconds = Randomizer.randomInt(-0, +59);
+                            const hours = this.randomizer.randomInt(-838, +838);
+                            const minutes = this.randomizer.randomInt(-0, +59);
+                            const seconds = this.randomizer.randomInt(-0, +59);
                             row[column.name] = `${hours}:${minutes}:${seconds}`;
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'year':
-                            row[column.name] = Randomizer.randomInt(column.options.min, column.options.max);
+                            row[column.name] = this.randomizer.randomInt(column.options.min, column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'varchar':
@@ -171,28 +179,28 @@ export class Generator {
                             if (column.options.max >= 36 && column.options.unique) {
                                 row[column.name] = uuid4(MersenneTwister19937.autoSeed());
                             } else {
-                                row[column.name] = Randomizer.randomString(Randomizer.randomInt(column.options.min as number, column.options.max));
+                                row[column.name] = this.randomizer.randomString(this.randomizer.randomInt(column.options.min as number, column.options.max));
                                 if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             }
                             break;
                         case 'tinyblob':
-                            row[column.name] = Randomizer.randomString(Randomizer.randomInt(0, 10));
+                            row[column.name] = this.randomizer.randomString(this.randomizer.randomInt(0, 10));
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'text':
                         case 'mediumtext':
                         case 'longtext':
-                            row[column.name] = Randomizer.randomString(Randomizer.randomInt(0, column.options.max));
+                            row[column.name] = this.randomizer.randomString(this.randomizer.randomInt(0, column.options.max));
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'blob':
                         case 'mediumblob': // 16777215
                         case 'longblob': // 4,294,967,295
-                            row[column.name] = Randomizer.randomString(Randomizer.randomInt(0, column.options.max));
+                            row[column.name] = this.randomizer.randomString(this.randomizer.randomInt(0, column.options.max));
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'set':
-                            row[column.name] = Randomizer.randomBit(column.options.max);
+                            row[column.name] = this.randomizer.randomBit(column.options.max);
                             if (column.options.nullable && Math.random() <= 0.1) row[column.name] = null;
                             break;
                         case 'enum':
--- src/generation/randomizer.ts
+++ src/generation/randomizer.ts
@@ -1,29 +1,36 @@
 import moment = require("moment");
 import { Random, MersenneTwister19937 } from "random-js";
-const random = new Random(MersenneTwister19937.autoSeed());
 
 export class Randomizer {
-    static randomBit(length: number) {
-        return Randomizer.randomInt(0, Math.pow(2, length));
+    private random: Random;
+
+    constructor(
+        seed?: number
+    ) {
+        if (seed) {
+            this.random = new Random(MersenneTwister19937.seed(seed));
+        } else {
+            this.random = new Random(MersenneTwister19937.autoSeed());
+        }
     }
 
-    static randomString(length: number) {
-        return random.string(length);
+    public randomBit(length: number) {
+        return this.randomInt(0, Math.pow(2, length));
     }
 
-    static randomInt(min: number = -127, max: number = 128) {
-        const tmpMin = Math.ceil(min);
-        const tmpMax = Math.floor(max);
-        return Math.floor(Math.random() * (tmpMax - tmpMin + 1)) + tmpMin;
+    public randomString(length: number) {
+        return this.random.string(length);
     }
 
-    static randomFloat(min: number = -127, max: number = 128) {
-        const tmpMin = Math.ceil(min);
-        const tmpMax = Math.floor(max);
-        return Math.random() * (tmpMax - tmpMin + 1) + tmpMin;
+    public randomInt(min: number = -127, max: number = 128) {
+        return this.random.integer(min, max);
     }
 
-    static randomDate(start = new Date('01-01-1970'), end = new Date()) {
-        return moment(new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))).format('Y-MM-DD HH:mm:ss');
+    public randomFloat(min: number = -127, max: number = 128) {
+        return this.random.real(min, max);
+    }
+
+    public randomDate(start = new Date('01-01-1970'), end = new Date()) {
+        return moment(this.random.date(start, end)).format('Y-MM-DD HH:mm:ss');
     }
 }
