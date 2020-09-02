c83b5feaf83a90b76b099d124aa3ed96ee987d38
Raphael67 <raphael@corteks.fr>
2020-09-02 13:54:25 +0200

    analyzing schema simply returns an array of tables

--- src/schema.interface.ts
+++ src/schema.interface.ts
@@ -1,22 +1,6 @@
 import { Table } from './table-descriptor.interface';
-import { databaseEngines } from './database-engines';
-import { ColumnOptions } from './column';
 
 export interface Schema {
-    settings: {
-        engine: databaseEngines;
-        disableTriggers: boolean;
-        ignoredTables: string[];
-        tablesToFill: string[];
-        values: { [key: string]: any[]; };
-        options: Array<
-            {
-                dataTypes: string[],
-                options: ColumnOptions;
-            }
-        >;
-        seed?: number;
-    };
     tables: Table[];
 }
 
