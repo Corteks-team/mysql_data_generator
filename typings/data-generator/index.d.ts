ea81ab85a1874ce48d965efa8afe06151b673d96
Raphael <raphael@corteks.fr>
2020-09-27 09:35:13 +0200

    remove obsolete interfaces replaced by classes

--- typings/data-generator/index.d.ts
+++ typings/data-generator/index.d.ts
@@ -3,8 +3,6 @@ type ParsedValues = string[];
 type ValuesWithRatio = { [key: string]: number; };
 type Values = ValuePointer | ParsedValues | ValuesWithRatio;
 
-type DatabaseEngines = 'MariaDB';
-
 interface MySQLColumn {
     TABLE_CATALOG: string;
     TABLE_SCHEMA: string;
@@ -52,92 +50,4 @@ interface Trigger {
     CHARACTER_SET_CLIENT: string,
     COLLATION_CONNECTION: string,
     DATABASE_COLLATION: string,
-}
-
-interface Schema {
-    tables: Table[];
-}
-
-interface Table {
-    name: string;
-    columns: Column[];
-    referencedTables: string[];
-}
-
-interface Column {
-    name: string;
-    generator: string;
-    options: {
-        nullable: boolean;
-        unique: boolean;
-        autoIncrement: boolean;
-        unsigned: boolean;
-        min: number;
-        max: number;
-        minDate?: string;
-        maxDate?: string | undefined;
-    };
-    foreignKey?: {
-        table: string;
-        column: string;
-        where?: string;
-    };
-    values?: Values;
-}
-
-interface CustomSchema {
-    settings: {
-        beforeAll: string[];
-        afterAll: string[];
-        engine: DatabaseEngines;
-        disableTriggers: boolean;
-        ignoredTables: string[];
-        tablesToFill: string[];
-        values: { [key: string]: any[]; };
-        options: Array<
-            {
-                dataTypes: string[],
-                options: Partial<Column['options']>;
-            }
-        >;
-        seed?: number;
-    };
-    tables: CustomTable[],
-}
-
-interface ForeignKey {
-    column: string,
-    foreignTable: string,
-    foreignColumn: string;
-    uniqueIndex: boolean;
-}
-
-interface DatabaseConnector {
-    init(): Promise<void>;
-    getTablesInformation(): Promise<Table[]>;
-    getColumnsInformation(table: Table): Promise<MySQLColumn[]>;
-    getForeignKeys(table: Table): Promise<ForeignKey[]>;
-    countLines(table: Table): Promise<number>;
-    emptyTable(table: Table): Promise<void>;
-    getValuesForForeignKeys(table: string, column: string, foreignTable: string, foreignColumn: string, limit: number, unique: boolean, condition: string | undefined): Promise<any[]>;
-    executeRawQuery(query: string): Promise<void>;
-    insert(table: string, lines: any[]): Promise<number>;
-    destroy(): Promise<void>;
-    backupTriggers(tables: string[]): Promise<void>;
-    cleanBackupTriggers(): void;
-    disableTriggers(table: string): Promise<void>;
-    enableTriggers(table: string): Promise<void>;
-}
-
-type CustomTable = { name: string; } & Partial<Table> & BaseTable;
-
-interface BaseTable {
-    name: string;
-    /** @deprecated: This parameter has been renamed maxLines */
-    lines?: number;
-    before?: string[];
-    after?: string[];
-    maxLines?: number;
-    addLines?: number;
-    disableTriggers?: boolean;
 }
