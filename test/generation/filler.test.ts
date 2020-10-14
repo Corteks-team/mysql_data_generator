8811c2c282aa40d01d176c3ad0981532dbf9bf7f
Raphael <raphael@corteks.fr>
2020-10-14 19:25:43 +0200

    test filler with seed

--- test/generation/filler.test.ts
+++ test/generation/filler.test.ts
@@ -1,8 +1,11 @@
 import { TestConnector } from '../test-connector';
 import { Filler } from '../../src/generation/filler';
 import { getLogger } from 'log4js';
-import { CustomizedSchema, CustomizedTable } from '../../src/schema/customized-schema.class';
+import { CustomizedSchema, CustomizedTable, CustomizedColumn } from '../../src/schema/customized-schema.class';
 import { Builder } from '../../src/builder';
+import { CustomSettings } from '../../src/schema/custom-schema.class';
+import { table } from 'console';
+import { Generators } from '../../src/generation/generators/generators';
 
 const logger = getLogger();
 let testConnector: TestConnector;
@@ -78,5 +81,41 @@ describe('Generator', () => {
         await generator.fillTables(true);
 
         expect(testConnector.executeRawQuery).toHaveBeenCalledWith('query');
+    });
+    it('should generate always same data with seed', async () => {
+        const column = new Builder(CustomizedColumn)
+            .set('name', 'col1')
+            .set('generator', Generators.string)
+            .build();
+
+        const customizedTable = new Builder(CustomizedTable)
+            .set('name', 'test')
+            .set('maxLines', 1)
+            .set('columns', [column])
+            .build();
+
+        const globalSettings = new Builder(CustomSettings)
+            .set('seed', 42)
+            .build();
+
+        const customizedSchema = new Builder(CustomizedSchema)
+            .set('settings', globalSettings)
+            .set('tables', [
+                customizedTable
+            ])
+            .build();
+
+        testConnector.insert = jest.fn(async (table, rows) => 1);
+
+        const generator = new Filler(
+            testConnector,
+            customizedSchema,
+            logger
+        );
+        await generator.fillTables(true);
+
+        expect(testConnector.insert).toHaveBeenCalledWith('test', [{
+            col1: 'ZCoQh8uM5swkkx0JNxcv0bxRDLb-7uGl5v8RyWA6_P-B7po99U9YR2Z-4cKYguiMrdy7nX5iz0btBU7gR8hUInqJXNdb9f1Pd1C_rz'
+        }]);
     });
 });
